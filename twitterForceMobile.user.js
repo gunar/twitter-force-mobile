45844b3dc2f243eb78e51ea22b0f1438429861f8
Gunar C. Gessner <gunar@gunargessner.com>
2016-10-26 11:24:52 -0200

    fix: add glob to match any twitter URL

--- twitterForceMobile.user.js
+++ twitterForceMobile.user.js
@@ -1,9 +1,12 @@
 // ==UserScript==
-// @name         Clean Google Drive
-// @version      1.0
-// @description  remove clutter from google drive
+// @name         Twittter Force Mobile
+// @version      1.0.1
+// @description  auto-forward you to the mobile version of twitter
 // @author       http://github.com/gunar
-// @match        https://drive.google.com/drive/*
+// @match        https://twitter.com/*
+// @match        https://www.twitter.com/*
+// @match        http://www.twitter.com/*
+// @match        http://twitter.com/*"
 // ==/UserScript==
 
 (function() {
