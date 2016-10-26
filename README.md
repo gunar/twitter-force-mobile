31a5b4746926ec6425282570fbd840b456cf159b
Gunar C. Gessner <gunar@gunargessner.com>
2016-10-26 11:17:26 -0200

    First commit :balloon:

---
+++ README.md
@@ -0,0 +1,7 @@
+# Twitter Force Mobile
+
+Chrome Extension to forward you to the mobile version of Twitter.com
+
+The preferred method of installation is through the [Extension](https://chrome.google.com/webstore/detail/hmfbabibadommpkjfnnebpgclobgdabf) at Google's Webstore.
+
+![screenshot](screenshot.png)
---
+++ twitterForceMobile.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Clean Google Drive
+// @version      1.0
+// @description  remove clutter from google drive
+// @author       http://github.com/gunar
+// @match        https://drive.google.com/drive/*
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  window.location = 'https://mobile.twitter.com' + window.location.pathname
+})();
